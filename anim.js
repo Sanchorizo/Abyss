e59b20d798792c9605542fb43895285f71857995
valentin <delomenie-v-87@hotmail.fr>
2023-06-27 15:33:45 +0200

    script dialogue

---
+++ anim.js
@@ -0,0 +1,6 @@
+var moz = document.getElementsByClassName("mozo")[0];
+
+moz.addEventListener("click", function () {
+    var diag = document.getElementsByClassName("dialogue")[0];
+    diag.classList.toggle("active");
+})
