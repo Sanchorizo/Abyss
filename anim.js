19c41c4c4c38fa876d4e09246b08d7e89205dc63
valentin <delomenie-v-87@hotmail.fr>
2023-06-28 12:55:37 +0200

    page mort

--- anim.js
+++ anim.js
@@ -1,13 +1,16 @@
 var moz = document.getElementsByClassName("mosa")[0];
 var diag = document.getElementsByClassName("dialogue")[0];
+var hidden = document.getElementsByClassName("hidden")[0];
 
 moz.addEventListener("click", function (event) {
     // Empêche la propagation du clic à l'élément de la page
     event.stopPropagation();
 
     diag.classList.toggle("active");
+    hidden.classList.toggle("active");
 });
 
 document.addEventListener("click", function () {
     diag.classList.remove("active");
+    hidden.classList.remove("active");
 });
